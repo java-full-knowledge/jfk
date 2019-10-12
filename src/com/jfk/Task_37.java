88705989fe54bcea96a8c0e84aa05cfa621ce219
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-12 16:36:35 +0400

    Task_37

---
+++ src/com/jfk/Task_37.java
@@ -0,0 +1,12 @@
+package com.jfk;
+
+public class Task_37 {
+    public static void main(String [] args)
+    {
+        int a=4,b=8,c=12,d=18;
+        if(d-c==c-b && c-b==b-a)
+            System.out.println(true);
+        else
+            System.out.println(false);
+    }
+}
