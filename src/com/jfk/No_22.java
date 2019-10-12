f574f4fa2a02787b6133988c4eb336028ac082f5
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-12 14:24:10 +0400

    Task_22

---
+++ src/com/jfk/No_22.java
@@ -0,0 +1,10 @@
+package com.jfk;
+
+public class No_22 {
+    public static void main(String [] args)
+    {
+        int a=22,b=43,c=21;
+        a=a<b ? a:b;
+        System.out.println(a<c?a:c);
+    }
+}
