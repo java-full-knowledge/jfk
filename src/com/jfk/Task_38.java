2031a9ceef5e06a9b6afba6e287aabb72bd742b3
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-12 16:38:24 +0400

    Task_38

---
+++ src/com/jfk/Task_38.java
@@ -0,0 +1,12 @@
+package com.jfk;
+
+public class Task_38 {
+    public static void main(String [] args)
+    {
+        int a=22,b=1,c=11,d=10;
+        if(d/c==c/b && c/b==b/a)
+            System.out.println(true);
+        else
+            System.out.println(false);
+    }
+}
