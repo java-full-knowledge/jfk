45624a404018b9537b6531952bd725c9ec1fb68f
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-12 15:45:09 +0400

    Task_32

---
+++ src/com/jfk/Task_32.java
@@ -0,0 +1,11 @@
+package com.jfk;
+
+public class Task_32 {
+    public static void main(String [] args)
+    {
+        int a=31,b=44,c=4,d=251;
+        a=a<b?a:b;
+        d=d<c?d:c;
+        System.out.println(a<d?a:d);
+    }
+}
