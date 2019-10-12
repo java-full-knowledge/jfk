f29e04a879222fef4346000ff79871e517d66e76
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-12 14:53:21 +0400

    Task_26

---
+++ src/com/jfk/Task_26.java
@@ -0,0 +1,12 @@
+package com.jfk;
+
+public class Task_26 {
+    public static void main(String [] args)
+    {
+        int a=23,b=1,c=71;
+        if(a%2==0 || b%2==0 || c%2==0)
+            System.out.println(1);
+        else
+            System.out.println(2);
+    }
+}
