4fd1fadc8751e0fe0857a9946673cba13f44fdc6
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-12 15:47:14 +0400

    Task_33

---
+++ src/com/jfk/Task_33.java
@@ -0,0 +1,12 @@
+package com.jfk;
+
+public class Task_33 {
+    public static void main(String [] args)
+    {
+        int a=22,b=1,c=71,d=1;
+        if(a==1 || b==1 || c==1 || d==1)
+            System.out.println(true);
+        else
+            System.out.println(false);
+    }
+}
