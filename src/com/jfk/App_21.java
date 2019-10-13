998e5f14e6278e7b36561be40ac234ed1207227d
MELO <meliktshiroyan@gmail.com>
2019-10-13 23:44:25 +0400

    work on Task 22

--- src/com/jfk/App_21.java
+++ src/com/jfk/App_21.java
@@ -3,6 +3,18 @@ package com.jfk;
 public class App_21 {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        int a = 1;
+        int b = 6;
+        int c = 8;
+        if (a > b && a > c){
+            System.out.println("Amenamecn e " + a);
+        }else{
+            if(b > a && b > c){
+                System.out.println(b);
+            }else{
+                System.out.println(c);
+            }
+        }
     }
+
 }
---
+++ src/com/jfk/App_22.java
@@ -0,0 +1,12 @@
+package com.jfk;
+
+public class App_22 {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 6;
+        int c = 8;
+if
+    }
+
+}
