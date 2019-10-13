df9e2c89ff42b1b1886c236dd8192aee8bd7b9e9
Stepan <ohanyan.stepan@gmail.com>
2019-10-13 18:35:19 +0400

    Task number 31

---
+++ src/com/jfk/App_31.java
@@ -0,0 +1,16 @@
+package com.jfk;
+
+
+/**
+ * @author Stepan Ohanyan
+ */
+
+
+public class App_31 {
+
+    public static void main(String[] args) {
+        byte a = 9, b = 7, c = 10, d = 18;
+
+    }
+
+}
