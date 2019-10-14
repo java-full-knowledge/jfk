2e1b3691d5a50cc18f7cd5ecd3571285566344b9
TigranAleksanyan <tigranperson1@gmail.com>
2019-10-14 19:34:32 +0400

    jjj

---
+++ src/com/jfk/App_21.java
@@ -0,0 +1,21 @@
+package com.jfk;
+
+
+
+public class App_21 {
+    public static void main(String[] args) {
+        int a = 31;
+        int b = 30;
+        int c = 56;
+        if(a > b && a > c){
+            System.out.println("mec e" + a);
+        }
+        else if (b > a && b > c) {
+            System.out.println(b);
+        }
+        else {
+            System.out.println(c);
+        }
+
+    }
+}
