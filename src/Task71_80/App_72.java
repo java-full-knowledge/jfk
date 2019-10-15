bbdddc5055e1f39b9b7ec43be56458755ce725a7
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-15 15:16:26 +0400

    Task from 71 to 80

--- src/Task71_80/App_72.java
+++ src/Task71_80/App_72.java
@@ -3,9 +3,10 @@ package Task71_80;
 public class App_72 {
     // y=ctg^2(x^2) x=[-5.4;1.2] ^x=0.4
     public static void main(String[] args) {
-        double y;
+        double y,m;
         for (double x = -5.40; x < 1.20; x += 0.40) {
-            y = (1/Math.tan(x*x))*(1/Math.tan(x*x));
+            m=Math.toRadians(x*x);
+            y = (1/Math.tan(m)*1/Math.tan(m));
             System.out.println("x=" + x + "   y=" + y);
         }
     }
