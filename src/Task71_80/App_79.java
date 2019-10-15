6d270667f562e7bac23b580df9d4dc830aabd38c
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-15 19:48:42 +0400

    Task from 71 to 80

---
+++ src/Task71_80/App_79.java
@@ -0,0 +1,36 @@
+package Task71_80;
+
+public class App_79 {
+    // x>1 ? y=log4x : y=-9 x=[-4;5] ^x=1
+    public static void main(String[] args) {
+
+        double y=0;
+
+        for(double x=-4;x<=5;x+=1)
+        {
+            if(x>1)
+            {
+
+                for(double i=1;i<=x;i+=1)
+                    if(Math.pow(4,i)==x )
+                    {
+
+                        y=i;
+                        break;
+                    }
+                    else
+                    if(Math.pow(4,i)>x)
+                    {
+                        y=(i-1);
+                        break;
+                    }
+
+            }
+            else
+                y=-9;
+            System.out.println("x="+(int)x+"    y="+(int)y);
+        }
+
+
+    }
+}
