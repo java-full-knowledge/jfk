55bd7c913c8be720ab75b0d98fd5289dc76cfec2
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-15 11:55:51 +0400

    Task from 51 to 70

---
+++ src/Task51_70/App_66.java
@@ -0,0 +1,11 @@
+package Task51_70;
+
+public class App_66 {
+    public static void main(String [] args)
+    {
+        String y;
+        int a=4134;
+        y=(a/1000==4 && a%10==4)?"YES":"NO";
+        System.out.println(y);
+    }
+}
