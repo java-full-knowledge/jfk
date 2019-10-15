38b158cabaec2990ec627187b4d44d00251c3745
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-15 12:06:32 +0400

    Task from 51 to 70

---
+++ src/Task51_70/App_68.java
@@ -0,0 +1,13 @@
+package Task51_70;
+
+public class App_68 {
+    public static void main(String [] args)
+    {
+
+        int a=4814;
+      if((a/10)%10<a%10)
+          System.out.println(((a/100)%10)*(a%10));
+      else
+          System.out.println(1);
+    }
+}
