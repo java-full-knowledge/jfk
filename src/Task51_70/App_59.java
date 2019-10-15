130a5a372fa851567b8832f4170fb942395c0d2c
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-15 11:20:27 +0400

    Task from 51 to 70

---
+++ src/Task51_70/App_59.java
@@ -0,0 +1,22 @@
+package Task51_70;
+
+public class App_59 {
+    public static void main(String [] args)
+    {
+        int a=697,min;
+       int [] arr=new int[]{a/100,(a/10)%10,a%10};
+       for(int i=0;i<arr.length-1;i++)
+       {
+           for(int j=1;j<arr.length;j++)
+                  if(arr[i]>arr[j]) {
+                      min = arr[i];
+                      arr[i] = arr[j];
+                      arr[j] = min;
+                  }
+
+       }
+       for(int b:arr)
+           System.out.println(b);
+
+    }
+}
