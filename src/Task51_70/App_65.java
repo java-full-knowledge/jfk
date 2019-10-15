2cb2c0da7d945e02d0e98bd44f15e2b19ba127e4
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-15 11:50:47 +0400

    Task from 51 to 70

---
+++ src/Task51_70/App_65.java
@@ -0,0 +1,11 @@
+package Task51_70;
+
+public class App_65 {
+    public static void main(String [] args)
+    {
+        String y;
+        int a=1132;
+        y=((a%10*(a/10)%10)==12)?"y=12":"y=0";
+        System.out.println(y);
+    }
+}
