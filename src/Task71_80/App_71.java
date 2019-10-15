7f0870927dcf0a1c190fd0b8fe3bee9fefc3aecf
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-15 15:15:44 +0400

    Task from 71 to 80

--- src/Task71_80/App_71.java
+++ src/Task71_80/App_71.java
@@ -3,9 +3,10 @@ package Task71_80;
 public class App_71 {
     // y=tg(2x+x^2),x=[2.4;7.6],^x=0.2
     public static void main(String [] args) {
-        double y;
+        double y,m;
         for (double x = 2.40; x < 7.70; x += 0.20) {
-            y=Math.tan(2*x+x*x);
+            m=Math.toRadians(2*x+x*x);
+            y=Math.tan(m);
             System.out.println("x=" +x+"   y="+y);
         }
 
