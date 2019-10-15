80c0c185b20f6ca732ff4fd4c47427e0ec15b3cd
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-15 11:46:39 +0400

    Task from 51 to 70

---
+++ src/Task51_70/App_64.java
@@ -0,0 +1,11 @@
+package Task51_70;
+
+public class App_64 {
+    public static void main(String [] args)
+    {
+        char y;
+        int a=1135;
+        y=((a%10+(a/10)%10)==5)?'s':'d';
+        System.out.println(y);
+    }
+}
