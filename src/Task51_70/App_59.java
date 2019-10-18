be5e66ed7f4a5981a0e49101520bfc68062354e5
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-18 19:03:58 +0400

    Task from 71 to 80

--- src/Task51_70/App_59.java
+++ src/Task51_70/App_59.java
@@ -3,8 +3,33 @@ package Task51_70;
 public class App_59 {
     public static void main(String [] args)
     {
-        int a=697,min;
-       int [] arr=new int[]{a/100,(a/10)%10,a%10};
+        int t=124;
+        int a=t/100,b=(t/10)%10,r=t%10;
+        if(a-b<0)
+        {
+            if(a-b>a-r)
+                System.out.println(a+" "+b+" "+r);
+            else
+            if(a-b<a-r && a-r<0)
+                System.out.println(a+" "+r+" "+b);
+            else
+            if( a-r>0)
+                System.out.println(r+" "+a+" "+b);
+        }
+        else
+        {
+            if(a-r>a-b)
+                System.out.println(r+" "+b+" "+a);
+            if(a-r<a-b && a-r>0)
+                System.out.println(b+" "+r+" "+a);
+            else
+            if(a-r<a-b )
+                System.out.println(b+" "+a+" "+r);
+        }
+
+        /*
+        int min;
+       int [] arr=new int[]{t/100,(t/10)%10,t%10};
        for(int i=0;i<arr.length-1;i++)
        {
            for(int j=1;j<arr.length;j++)
@@ -17,6 +42,8 @@ public class App_59 {
        }
        for(int b:arr)
            System.out.println(b);
+
+       */
 
     }
 }
