008df49f20182ca31de66f32881a8100e44c6409
TigranAleksanyan <tigranperson1@gmail.com>
2019-10-18 13:22:57 +0400

    app55

---
+++ src/com/jfk/App56.java
@@ -0,0 +1,20 @@
+package com.jfk;
+
+import java.util.Arrays;
+
+public class App56 {
+    public static void main(String[] args) {
+        int x = 864;
+        int a = x / 100;
+        int b = (x / 10) % 10;
+        int c = x % 10;
+         float y = (a + b + c) / x;
+        if (c > b) {
+            System.out.println(y);
+
+        } else {
+            System.out.println(x);
+
+        }
+    }
+}
