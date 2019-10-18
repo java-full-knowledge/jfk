acce90ddeef5f83392badc64473776a74346eac3
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-18 17:33:02 +0400

    Task from 71 to 80

--- src/Task51_70/App_60.java
+++ src/Task51_70/App_60.java
@@ -1,22 +1,49 @@
 package Task51_70;
 
 public class App_60 {
+
     public static void main(String [] args)
     {
-        int a=697,min;
-        int [] arr=new int[]{a/100,(a/10)%10,a%10};
-        for(int i=0;i<arr.length-1;i++)
+        int t=697;
+        int a=t/100,b=(t/10)%10,r=t%10;
+        if(a-b>0)
         {
-            for(int j=1;j<arr.length;j++)
-                if(arr[i]<arr[j]) {
-                    min = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = min;
-                }
-
+            if(a-b<a-r)
+                System.out.println(a+" "+b+" "+r);
+            else
+            if(a-b>a-r && a-r>0)
+                System.out.println(a+" "+r+" "+b);
+            else
+            if(a-b>a-r && a-r<0)
+                System.out.println(r+" "+a+" "+b);
         }
-        for(int b:arr)
-            System.out.println(b);
-
+        else {
+            if (a - r < a - b)
+                System.out.println(r + " " + b + " " + a);
+            if (a - r > a - b && a - r < 0)
+                System.out.println(b + " " + r + " " + a);
+            else if (a - r > a - b && a - r > 0)
+                System.out.println(b + " " + a + " " + r);
+        }
     }
+//Array
+        /*
+        int min;
+       int [] arr=new int[]{t/100,(t/10)%10,t%10};
+       for(int i=0;i<arr.length-1;i++)
+       {
+           for(int j=1;j<arr.length;j++)
+                  if(arr[i]>arr[j]) {
+                      min = arr[i];
+                       arr[i] = arr[j];
+                      arr[j] = min;
+                  }
+
+       }
+       for(int b:arr)
+           System.out.println(b);
+
+       */
+
 }
+
