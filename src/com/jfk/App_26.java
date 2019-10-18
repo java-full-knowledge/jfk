0f9aa70043b1cc63c29ad3785ee5f9cf7ecf7d30
Davit <mndavit@gmail.com>
2019-10-18 15:10:02 +0400

    App_26

---
+++ src/com/jfk/App_26.java
@@ -0,0 +1,16 @@
+package com.jfk;
+
+public class App_26 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 71;
+        int c = 19;
+        if ((a / 2 == 0) || (b / 2 == 0) || (c / 2 == 0))
+        {
+            System.out.println("1");
+        }
+        else {
+            System.out.println("2");
+        }
+    }
+}
