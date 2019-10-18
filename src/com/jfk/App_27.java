bc90b9092a774c2e2c15156a833745469c2f0030
Davit <mndavit@gmail.com>
2019-10-18 15:16:44 +0400

    App_27

---
+++ src/com/jfk/App_27.java
@@ -0,0 +1,17 @@
+package com.jfk;
+
+public class App_27 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        int c = 8;
+        boolean x = true;
+        if ((a - b) == (b - c))
+        {
+            System.out.println(x);
+        }
+        else {
+            System.out.println(false);
+        }
+    }
+}
