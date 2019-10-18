1a2021df843b0304645e75faa033b6b55044bf03
Davit <mndavit@gmail.com>
2019-10-18 14:43:18 +0400

    Test

--- src/com/jfk/App_22.java
+++ src/com/jfk/App_22.java
@@ -2,15 +2,15 @@ package com.jfk;
 
 public class App_22 {
     public static void main(String[] args) {
-        int a = 20;
-        int b = -3;
-        int c = 4;
+        int a = 11;
+        int b = 0;
+        int c = 7;
         if (a < b && a < c) {
             System.out.println(a   + " is the smallest number");
         } else if (b < a && b < c) {
-            System.out.println(b + "is the smallest number");
+            System.out.println(b + " is the smallest number");
         } else if (c < a && c < b) {
-            System.out.println(c + "is the smallest number");
+            System.out.println(c + " is the smallest number");
         }
     }
 }
---
+++ src/com/jfk/App_23.java
@@ -0,0 +1,17 @@
+package com.jfk;
+
+public class App_23 {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 33;
+        int c = 33;
+        boolean x = true;
+       if ((a == b) || (a == b)|| (b == c))
+            {
+           System.out.println(x);
+            }
+       else {
+           System.out.println(false);
+            }
+        }
+    }
