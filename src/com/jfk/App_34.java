5585222a7896d02ccd418aea44f0cb6eabdffe9d
Davit <mndavit@gmail.com>
2019-10-18 17:52:02 +0400

    App_32

---
+++ src/com/jfk/App_34.java
@@ -0,0 +1,28 @@
+package com.jfk;
+
+public class App_34 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        int c = 11;
+        int d = 12;
+        boolean x = true;
+        if ((a + b) == (c + d))
+        {
+            System.out.println(x);
+        }
+        else if ((a + c) == (b + d) )
+        {
+            System.out.println(x);
+        }
+        else if((a + d) == (b + c))
+        {
+            System.out.println(x);
+        }
+        else {
+            System.out.println(false);
+        }
+        }
+
+    }
+
