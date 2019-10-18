5f6c2ffe4cbf489315f2cea90303770578ced74b
tvardanyan <vardanyantg@gmail.com>
2019-10-18 16:50:49 +0400

    Ex74

---
+++ src/com/jfk/app074.java
@@ -0,0 +1,12 @@
+package com.jfk;
+
+public class app074 {
+    public static void main(String[] args) {
+        double x = -3.8 , a = 0.3 , y;
+        while (x < 5.4){
+            y = (Math.pow(2,(x + 4)));
+            x = x + a;
+            System.out.println(y);
+        }
+    }
+}
