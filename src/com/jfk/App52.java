62bfa39ff99ff1eecbaa096a9c924cad7f1e7173
TigranAleksanyan <tigranperson1@gmail.com>
2019-10-18 10:41:45 +0400

    APP 52

---
+++ src/com/jfk/App52.java
@@ -0,0 +1,19 @@
+package com.jfk;
+
+
+public class App52 {
+    public static void main(String[] args) {
+        boolean t = false;
+        int k = 655;
+        int a = k/100;
+        int b = (k/10) % 10;
+        int c = k % 10;
+        if (a == b  || b ==c || a==c ) {
+            t = true;
+            System.out.println(t);}
+    }
+
+    }
+
+
+
