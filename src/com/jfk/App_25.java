10a43ebc35436044a9e8bc155774bd8b34ed236c
Davit <mndavit@gmail.com>
2019-10-18 15:05:34 +0400

    App_25

---
+++ src/com/jfk/App_25.java
@@ -0,0 +1,16 @@
+package com.jfk;
+
+public class App_25 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 3;
+        int c = -1;
+        if ((a > 0) && (b > 0) && (c > 0))
+        {
+            System.out.println("y = 1");
+        }
+        else {
+            System.out.println("y = 2");
+        }
+    }
+}
