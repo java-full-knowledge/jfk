12b6c89c1f8a1ac8105cec8e77dae722edf63737
Davit <mndavit@gmail.com>
2019-10-18 14:50:22 +0400

    App_24

--- src/com/jfk/App_24.java
+++ src/com/jfk/App_24.java
@@ -4,9 +4,9 @@ public class App_24 {
     public static void main(String[] args) {
         int a = 2;
         int b = 33;
-        int c = 2;
+        int c = 1;
         boolean x = true;
-        if ((a == b) && (a == b)|| (b == c))
+        if ((a == b) && (a == c)|| (b == c))
         {
             System.out.println(x);
         }
