ebfd33c9a61f48c5b190b40a9022e896aa49aaac
Davit <mndavit@gmail.com>
2019-10-18 17:31:06 +0400

    App_32

---
+++ src/com/jfk/App_33.java
@@ -0,0 +1,17 @@
+package com.jfk;
+
+public class App_33 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int c = 4;
+        int d = 1;
+        boolean x = true;
+
+        if ((a == 1) || (b == 1) || (c == 1) || (d == 1) )
+        {
+            System.out.println(x);
+        }
+        else System.out.println(false);
+    }
+}
