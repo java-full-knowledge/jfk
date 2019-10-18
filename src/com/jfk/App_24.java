b4e5dbf534a3a3ddf36393aca123e3f2f08946f5
Davit <mndavit@gmail.com>
2019-10-18 14:56:48 +0400

    App_24

--- src/com/jfk/App_24.java
+++ src/com/jfk/App_24.java
@@ -2,16 +2,25 @@ package com.jfk;
 
 public class App_24 {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 33;
-        int c = 1;
+        int a = 22;
+        int b = 2;
+        int c = 2;
         boolean x = true;
-        if ((a == b) && (a == c)|| (b == c))
+        if ((a == 2) && (c == 2))
         {
             System.out.println(x);
         }
-        else {
+        else if ((a == 2) && (b == 2))
+                {
+                    System.out.println(x);
+                 }
+        else if ((b == 2) && (c == 2))
+                {
+                    System.out.println(x);
+                }
+        else
+            {
             System.out.println(false);
-        }
+            }
     }
 }
