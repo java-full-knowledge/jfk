3384be8bfed1d9fb85f0bb0dbe9aae9d3d949c2a
Davit <mndavit@gmail.com>
2019-10-18 15:42:08 +0400

    App_27

---
+++ src/com/jfk/App_28.java
@@ -0,0 +1,17 @@
+package com.jfk;
+
+public class App_28 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = -3;
+        int c = 9;
+        int r = (Math.abs (c)) / (Math.abs(b));
+        boolean x = true;
+        if (Math.abs(b) == r) {
+            System.out.println(x);
+        }
+        else {
+            System.out.println(false);
+        }
+    }
+}
