ad98209fb78b1b012128d05821e29b906f7bbe9c
TigranAleksanyan <tigranperson1@gmail.com>
2019-10-18 17:45:57 +0400

    APP60

---
+++ src/com/jfk/APP59.java
@@ -0,0 +1,41 @@
+package com.jfk;
+
+public class APP59 {
+    public static void main(String[] args) {
+        int x = 436;
+        int a = x / 100;
+        int b = (x / 10) % 10;
+        int c = x % 10;
+        if (a > b && a > c && b > c) {
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+        } else if (b > a && b > c && c > a) {
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(a);
+        } else if (a > b && a > c && c > b) {
+            System.out.println(a);
+            System.out.println(c);
+            System.out.println(b);
+        } else if (b > a && b > c && a > c) {
+            System.out.println(b);
+            System.out.println(a);
+            System.out.println(c);
+        } else if (c > a && c > b && a > b){
+            System.out.println(c);
+            System.out.println(a);
+            System.out.println(b);}
+            else if (c>a && c>b && a<b){
+                System.out.println(c);
+                System.out.println(b);
+                System.out.println(a);}
+        }
+    }
+
+
+
+
+
+
+
