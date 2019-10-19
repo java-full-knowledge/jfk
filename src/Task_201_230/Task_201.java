a348fcf2776ed9b3e9d885cb3e21e7ab12b8c056
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-20 01:02:51 +0400

    Task from 201 to 230

---
+++ src/Task_201_230/Task_201.java
@@ -0,0 +1,13 @@
+package Task_201_230;
+
+public class Task_201 {
+    public  static  void  main(String [] args) {
+        int n=23435435,s=0;
+        while(n!=0)
+        {
+            s++;
+            n=n/10;
+        }
+        System.out.println(s);
+    }
+}
