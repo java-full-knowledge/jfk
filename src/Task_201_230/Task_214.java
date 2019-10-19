e442af859cd6be7887958c02baf70075de36dbdc
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-20 01:11:58 +0400

    Task from 201 to 230

---
+++ src/Task_201_230/Task_214.java
@@ -0,0 +1,14 @@
+package Task_201_230;
+
+public class Task_214 {
+    public  static  void  main(String [] args) {
+        int[] arr = new int[]{23, 4, 5, 66, 7, -4, -55, -66, 2, -33, -5};
+        int count = 0, s = 0;
+        for (int i : arr)
+            if (i < 0) {
+                s += i;
+                count++;
+            }
+        System.out.println((double)s / count);
+    }
+}
