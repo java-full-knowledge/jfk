7895f30a0d0f5594177b0c98ea578cbc4fd8cb00
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-20 22:20:38 +0400

    Task from 151 to 173

---
+++ src/Task_151_173/Task_152.java
@@ -0,0 +1,11 @@
+package Task_151_173;
+
+public class Task_152 {
+    public  static  void  main(String [] args) {
+        int n=128,s=1;
+        for(int i=1;i<=n;i++)
+            if(n%i==0)
+                s*=i;
+        System.out.println("s="+s);
+    }
+}
