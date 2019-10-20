f8ae5d1c988dc904753ba7d9dab76534efdc01bb
ATorosyan1 <arman_torosyan_1997@mail.ru>
2019-10-20 22:23:04 +0400

    Task from 151 to 173

---
+++ src/Task_151_173/Task_155.java
@@ -0,0 +1,11 @@
+package Task_151_173;
+
+public class Task_155 {
+    public  static  void  main(String [] args) {
+        int s=0;
+        for(int i=10;i<=99;i++)
+            if(i%3==0)
+                s+=i;
+        System.out.println("s="+s);
+    }
+}
