ea024475ca3698d1a466d3b27b5af0e5e037e341
MELO <meliktshiroyan@gmail.com>
2019-10-21 14:48:41 +0400

    App-161

---
+++ src/com/jfk/App_161.java
@@ -0,0 +1,23 @@
+package com.jfk;
+
+public class App_161 {
+    public  static  void  main(String [] args) {
+        int a;
+        double b;
+        boolean t=false;
+        for(double i=1000;i<=9999;i++)
+        {
+            a = (int) Math.sqrt(i * 26);
+            b=Math.sqrt(i * 26);;
+            if((double) a==b) {
+                t=true;
+                System.out.println((int)i);
+                break ;
+            }
+        }
+        if(t==false)
+            System.out.println("False");
+
+    }
+}
+
