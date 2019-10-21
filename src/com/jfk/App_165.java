abd45aad5e33b54c9b7b82a821fa3cf569f48e69
MELO <meliktshiroyan@gmail.com>
2019-10-21 16:03:02 +0400

    App-163

---
+++ src/com/jfk/App_165.java
@@ -0,0 +1,18 @@
+package com.jfk;
+
+public class App_165 {
+    public static void main(String[] args) {
+        int n = 8;
+        int a = (int) Math.cbrt(n);
+        double b = Math.cbrt(n);
+        boolean t = true;
+        if (a == b) {
+
+            System.out.println(t);
+        } else {
+            System.out.println(" False");
+
+
+        }
+    }
+}
