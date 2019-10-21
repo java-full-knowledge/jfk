9137e942999e0798ec8bee37eb1b717ffb9890b0
MELO <meliktshiroyan@gmail.com>
2019-10-21 17:58:53 +0400

    App-203

---
+++ src/com/jfk/App_203.java
@@ -0,0 +1,19 @@
+package com.jfk;
+
+public class App_203 {
+    public static void main(String[] args) {
+        int number = 23435353;
+        int count = 0;
+        while (number != 0) {
+
+            int x = number%10;
+
+            number= number/10;
+
+            count *= x;
+            count++;
+        }
+
+        System.out.println("Count : " + count);
+    }
+}
