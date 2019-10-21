606122d26087bc78fab5d15b83d929a06ba6b76a
TigranAleksanyan <tigranperson1@gmail.com>
2019-10-21 18:58:34 +0400

    ggghhh

--- src/com/jfk/APP154.java
+++ src/com/jfk/APP154.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class APP154 {
     public static void main(String[] args) {
-        int n=100;
+        int n=100,t=0;
         int mult=1;
         for(int i=1; i <=100; i++ )
             if(n%i==3)
