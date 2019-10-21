fab93a476701142d77783b26d1d54bac1c2357b3
TigranAleksanyan <tigranperson1@gmail.com>
2019-10-21 17:35:09 +0400

    APP154

---
+++ src/com/jfk/APP154.java
@@ -0,0 +1,14 @@
+package com.jfk;
+
+import java.util.Arrays;
+
+public class APP154 {
+    public static void main(String[] args) {
+        int n=100;
+        int mult=1;
+        for(int i=1; i <=100; i++ )
+            if(n%i==3)
+             mult*=i;
+        System.out.println("mult = " + mult);
+    }
+}
