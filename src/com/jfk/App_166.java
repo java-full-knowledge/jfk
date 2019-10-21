ebd511110ed9dbd783fac5f7b1870af439ac2e07
MELO <meliktshiroyan@gmail.com>
2019-10-21 17:04:21 +0400

    App-166

---
+++ src/com/jfk/App_166.java
@@ -0,0 +1,19 @@
+package com.jfk;
+
+public class App_166 {
+    public static void main(String[] args) {
+        int n = 15;
+        int y = 0;
+        int a = (int) Math.pow(n, 1.0 / 4);
+        double b =  Math.pow(n, 1.0 / 4);
+        if ((double)a != b) {
+            System.out.println(y);
+        } else {
+
+            System.out.println("1");
+        }
+
+    }
+}
+
+
