bc8d86a025635f9f76dec7ac849ad3d7ef278735
stepanohanyan <ohanyan.stepan@gmail.com>
2019-10-23 18:59:06 +0400

    Tasks from 257 to 260

--- src/com/jfk/tasks_251_260/App_258.java
+++ src/com/jfk/tasks_251_260/App_258.java
@@ -3,7 +3,19 @@ package com.jfk.tasks_231_250;
 public class App_258 {
 
     public static void main(String[] args) {
+        int[] arr = {18, 11, 51, 11, 22, 51};
+        int index = 0;
+        int max = arr[0];
 
+
+        for(int i = 1; i < arr.length; i++) {
+            if (arr[i] >= max) {
+                max = arr[i];
+                index = i;
+            }
+        }
+
+        System.out.println("Index: " + index);
     }
 
 }
--- src/com/jfk/tasks_251_260/App_260.java
+++ src/com/jfk/tasks_251_260/App_260.java
@@ -3,7 +3,19 @@ package com.jfk.tasks_231_250;
 public class App_260 {
 
     public static void main(String[] args) {
+        int[] arr = {18, 11, 51, 11, 22, 51};
+        int index = 0;
+        int min = arr[0];
 
+
+        for(int i = 1; i < arr.length; i++) {
+            if (arr[i] <= min) {
+                min = arr[i];
+                index = i;
+            }
+        }
+
+        System.out.println("Index: " + index);
     }
 
 }
