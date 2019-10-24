9694fcad1dec0f27b87019c39757fcf422380886
MELO <meliktshiroyan@gmail.com>
2019-10-24 22:25:21 +0400

    App-202

--- src/com/jfk/App_202.java
+++ src/com/jfk/App_202.java
@@ -2,18 +2,12 @@ package com.jfk;
 
 public class App_202 {
     public static void main(String[] args) {
-        int number = 23435353;
-        int count = 0;
-        while (number != 0) {
-
-            int x = number%10;
-
-            number= number/10;
-
-            count += x;
-            count++;
+        int n = 234125;
+        int i = 0;
+        while (n != 0) {
+            i += n%10;
+            n=n/10;
         }
-
-        System.out.println("Count : " + count);
+        System.out.println("Count : " + i);
     }
 }
