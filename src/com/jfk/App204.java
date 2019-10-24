7c5edffdca272c2703a8c07cc24366248d425231
TigranAleksanyan <tigranperson1@gmail.com>
2019-10-24 22:42:59 +0400

    APP204

---
+++ src/com/jfk/App204.java
@@ -0,0 +1,15 @@
+package com.jfk;
+
+import java.util.Arrays;
+
+public class App204 {
+    public static void main(String[] args) {
+        int n=8569;
+        int num=0;
+        while(n!=0){
+            num=n%10;
+            n=n/10;
+            System.out.println("num = " + num);
+        }
+    }
+}
