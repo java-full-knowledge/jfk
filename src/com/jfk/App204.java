978fb5a035c97b964b3c845b9f587d63f624680f
TigranAleksanyan <tigranperson1@gmail.com>
2019-10-24 23:08:55 +0400

    APP204.1

--- src/com/jfk/App204.java
+++ src/com/jfk/App204.java
@@ -9,7 +9,7 @@ public class App204 {
         while(n!=0){
             num=n%10;
             n=n/10;
-            System.out.println("num = " + num);
+            System.out.println(num);
         }
     }
 }
