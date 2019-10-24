c9bfd734a93c2e5a8a5ddefc69147b95bb4b752a
MELO <meliktshiroyan@gmail.com>
2019-10-24 23:07:33 +0400

    App-206

---
+++ src/com/jfk/App_206.java
@@ -0,0 +1,13 @@
+package com.jfk;
+
+public class App_206 {
+    public static void main(String[] args) {
+        int n = 2341;
+        int i = 0;
+        while (n != 0) {
+            i = n%10;
+            System.out.print(i);
+            n=n/10;}
+    }
+}
+
