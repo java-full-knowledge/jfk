94152c02ea5c349bc7089b0104a272e13e5ffba0
TigranAleksanyan <tigranperson1@gmail.com>
2019-10-27 02:09:29 +0400

    APP160

---
+++ src/com/jfk/App160.java
@@ -0,0 +1,16 @@
+package com.jfk;
+
+public class App160 {
+    public static void main(String[] args) {
+         int n ;
+        for ( float i = 100; i < 1000; i++) {
+            n=(int)Math.sqrt(i*16);
+            System.out.println((int)i);
+break;
+        }
+        
+
+
+
+    }
+}
