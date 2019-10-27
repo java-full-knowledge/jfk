6f5a0db2a79895ac1a034668719e5f63d3b5b7a3
MELO <meliktshiroyan@gmail.com>
2019-10-28 01:53:18 +0400

    App-212

---
+++ src/com/jfk/App_212.java
@@ -0,0 +1,15 @@
+package com.jfk;
+
+public class App_212 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, -5, 15, 25, 32};
+        int k = 0, s = 0;
+        for (int i : arr) {
+            if (i > 0) {
+                s = s + (i * i);
+                k++;
+            }
+        }
+        System.out.println((double) s / k);
+    }
+}
