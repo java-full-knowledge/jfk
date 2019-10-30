371ab24b4c01bc86468b800abc0e14566bd6c62c
MELO <meliktshiroyan@gmail.com>
2019-10-31 00:40:22 +0400

    219

---
+++ src/com/jfk/App_219.java
@@ -0,0 +1,13 @@
+package com.jfk;
+
+public class App_219 {
+    public static void main(String[] args) {
+        int []arr = new int []{ 1, 3, 5, 4, 6, 10, 12, -16};
+        int s = 0, k =2;
+        for ( int i = 0; i < arr.length; i++){
+            if ( arr[i] % k ==0 );
+            s = arr[i]+s;}
+            System.out.println(s);
+        }
+    }
+
