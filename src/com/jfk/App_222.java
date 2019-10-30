93a6b0e4b16ba5bc64b388b2e932cdb5130685bc
MELO <meliktshiroyan@gmail.com>
2019-10-31 01:25:49 +0400

    222

---
+++ src/com/jfk/App_222.java
@@ -0,0 +1,14 @@
+package com.jfk;
+
+public class App_222 {
+    public static void main(String[] args) {
+       int c = 1, d = 12;
+       int s = 1;
+        int[]arr= new int []{ c, 3, 5, 4, 1, -10, 2, d};
+        for (int i =0; i< arr.length; i++){
+            if (arr[i]>=c && arr[i]<= d);
+            s = arr[i]* s;}
+        System.out.println(s);
+        }
+
+    }
