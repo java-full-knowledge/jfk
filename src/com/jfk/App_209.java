d3c9a35157182429fc0773bd728993c6b1405a79
MELO <meliktshiroyan@gmail.com>
2019-10-30 23:08:41 +0400

    209

---
+++ src/com/jfk/App_209.java
@@ -0,0 +1,16 @@
+package com.jfk;
+
+public class App_209 {
+    public static void main(String[] args) {
+        int n =2356478,k = n%10;
+        boolean i = false;
+        while ( n != 0){
+            if (n% 10 != 0 ){
+                i = true;
+        }
+        n= n/10;}
+                System.out.println( i);
+
+        }
+    }
+
