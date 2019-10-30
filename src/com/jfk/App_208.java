e260f6dc4d7744b05db9a5ddd50522f1e52314e0
MELO <meliktshiroyan@gmail.com>
2019-10-30 23:01:24 +0400

    208

---
+++ src/com/jfk/App_208.java
@@ -0,0 +1,19 @@
+package com.jfk;
+
+public class App_208 {
+    public static void main(String[] args) {
+        int n = 2234111;
+        boolean i = true;
+        int   k = n%10;
+        while (n != 0) {
+            if (k != n % 10) {
+                i = false;
+                break;
+            }
+            n=n/10;}
+        System.out.println( i );
+
+
+    }
+    }
+
