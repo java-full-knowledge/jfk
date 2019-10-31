9521a152972149ca538c270e163a783566013925
MELO <meliktshiroyan@gmail.com>
2019-11-01 01:11:35 +0400

    234

---
+++ src/com/jfk/App_234.java
@@ -0,0 +1,14 @@
+package com.jfk;
+
+public class App_234 {
+    public static void main(String[] args) {
+        int []arr= new int[]{ 1, 3, 5, 4, 1, -10, 2, -15};
+        int s = 1, k= 1;
+        for ( int i =0; arr.length>i; i++){
+            if ( arr[i] % 2 != 0);
+            s = s+ arr[i]; }
+        k++;
+        System.out.println(" gumar" + (int) s/k);
+        }
+    }
+
