3567bf759cbf35ed6dc7c405e616bfe3d55edc2d
MELO <meliktshiroyan@gmail.com>
2019-11-01 01:20:40 +0400

    235

---
+++ src/com/jfk/App_235.java
@@ -0,0 +1,15 @@
+package com.jfk;
+
+public class App_235 {
+    public static void main(String[] args) {
+        int[]arr= new int []{1, 3, 5, 4, 1, -10, 2, -15};
+        int s = 0, k = 0;
+        for (int i = 0; i< arr.length; i++) {
+            if (arr[i] / 2 != 0) ;
+            s = arr[i]*arr[i] + s;
+            k++;}
+            System.out.println("gumar  "+(int) s/k);
+        }
+        }
+
+
