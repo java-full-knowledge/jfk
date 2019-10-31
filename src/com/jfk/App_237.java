1afdaad1ee561e37372573bdf16646c712d4270b
MELO <meliktshiroyan@gmail.com>
2019-11-01 01:50:41 +0400

    237

---
+++ src/com/jfk/App_237.java
@@ -0,0 +1,13 @@
+package com.jfk;
+
+public class App_237 {
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1, 0, 5, 4, 1, -10, 2, -15};
+        int S=0;
+        for(int i:arr)
+            if(i==0)
+                S ++;
+        System.out.println("Gum: "+S);
+        }
+    }
