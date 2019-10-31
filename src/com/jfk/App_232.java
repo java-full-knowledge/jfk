7d754992b0dc1b87504fdc8dd2d1879bbc334137
MELO <meliktshiroyan@gmail.com>
2019-11-01 01:40:03 +0400

    232

---
+++ src/com/jfk/App_232.java
@@ -0,0 +1,17 @@
+package com.jfk;
+
+public class App_232 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 3, 5, 4, 1, -10, 2, -15};
+        int s = 0, k = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) ;
+            k++;
+        }
+            System.out.println(" qanak  " + k);
+        }
+    }
+
+
+
+
