0da504d396549a8c92e6eb8b5faa44c3cb02ad61
SMovsisyan <56359079+SMovsisyan@users.noreply.github.com>
2019-11-01 19:08:38 +0400

    Task 51-70

---
+++ src/com/jfk/character/Man.java
@@ -0,0 +1,36 @@
+
+package src.com.jfk.character;
+
+public class Man {
+
+    String name;
+    int age;
+    String designation;
+    double salary;
+
+    public Man(String name) {
+        this.name = name;
+    }
+
+    public void ManAge(int ManAge) {
+        age = ManAge;
+    }
+
+    public void ManDesignation(String ManDesig) {
+        designation = ManDesig;
+    }
+
+    public void ManSalary(double ManSalary) {
+        salary = ManSalary;
+    }
+
+    public void printMan() {
+        System.out.println("Name:" + name);
+        System.out.println("Age:" + age);
+        System.out.println("Designation:" + designation);
+        System.out.println("Salary:" + salary);
+    }
+}
+
+
+
