de004d1f0f44ea067a415fea909c8404381d7f54
MELO <meliktshiroyan@gmail.com>
2019-11-18 00:28:41 +0400

    Date_test

---
+++ src/com/jfk/Date_Test.java
@@ -0,0 +1,12 @@
+package com.jfk;
+
+
+public class Date_Test {
+    public static void main(String [] args)
+    {
+Date_java Test11 = new Date_java(10,12,1990);
+        System.out.println(Test11);
+        Test11.setDate(25,12,1991);
+        System.out.println(Test11.getYear());
+    }
+}
